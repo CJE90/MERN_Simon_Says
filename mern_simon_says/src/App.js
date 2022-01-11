ec7c1d15fdaf7d91baa7f2fb5b881fd0fee2e9cf
CJE90 <mrktup@hotmail.com>
2022-01-10 20:01:32 -0500

    forgot to save changes to App.js

--- mern_simon_says/src/App.js
+++ mern_simon_says/src/App.js
@@ -19,7 +19,6 @@ const App = () => {
         handleUsernameChange={({ target }) => setUsername(target.value)}
         handlePasswordChange={({ target }) => setPassword(target.value)}
         handleSubmit={handleLogin} />
-
     )
   }
 
