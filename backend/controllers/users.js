05305e6426d61ebc3b4d893c09ef2562b6f8dddd
CJE90 <mrktup@hotmail.com>
2022-01-15 13:25:06 -0500

    Adding user schmea and initial put and get commands

---
+++ backend/controllers/users.js
@@ -0,0 +1,29 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+    const user = User({
+        username: body.username,
+        passwordHash,
+        highestScore: 0,
+        lastScore: 0,
+        gamesPlayed: 0
+    })
+
+    const savedUser = await user.save()
+
+    response.json(savedUser)
+})
+
+module.exports = usersRouter
--- backend/models/user.js
+++ backend/models/user.js
@@ -1,27 +1,25 @@
 const mongoose = require('mongoose')
 
-const highScoreSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
         minlength: 3
     },
-    score: {
-        type: String,
-        required: true
-    },
-    playerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'PlayerID'
-    }
+    passwordHash: String,
+    highestScore: String,
+    lastScore: String,
+    gamesPlayed: String
+
 })
 
-highScoreSchema.set('toJSON', {
+userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
+        delete returnedObject.passwordHash
     }
 })
 
-module.exports = mongoose.model('HighScore', highScoreSchema)
+module.exports = mongoose.model('User', userSchema)
